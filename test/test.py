f16fb3010bfd492e3818016de7671c6bbddf9663
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-16 22:41:56 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -3,24 +3,19 @@
 
 import sys
 from barber import barber
+from censorx import censorx
 
 sys.path.insert(0, './')
 sys.path.insert(0, '../')
 
-from censorx import censorx
-
 inputFileOne = open("./test.cer", "rb")
-
 inputFileTwo = open("./test.cer", "rb")
 
 print("\nBefore: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n")
-
 print(barber.cut(inputFileOne.read().hex()))
 
 print("\nAfter: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n")
 
 decoder = censorx.decoder()
-
 decoder.scan(inputFileTwo)
-
 decoder.print()
