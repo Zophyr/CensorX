e6463245956964bd7ac9f3cf96347699ab4cbdde
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-16 17:44:01 +0800

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+
+sys.path.insert(0, './')
+sys.path.insert(0, '../')
+
+from CensorX import censorx
+
+inputFile = open("test/test.cer", "rb")
+
+decoder = censorx.decoder()
+
+decoder.scan(inputFile)
+
+decoder.print()
