bdeed374de0bc266483b4c4396ef664f60e1d20b
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-16 20:52:15 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='censorx',
+      version='0.0.2',
+      description='X.509 数字证书解析',
+      author='Zophyr',
+      author_email='the-zephyr@hotmail.com',
+      url='https://zophyr.com',
+      packages=['censorx'],
+      )
